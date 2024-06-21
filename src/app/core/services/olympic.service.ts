5695ab1f4189a4f537901ec938772d0f748c1100
Ali <zaabat97@gmail.com>
2024-06-21 11:33:18 +0200

    scroll withdrawal in the detail page

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -17,12 +17,9 @@ export class OlympicService {
     return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error) => {
-        // TODO: improve error handling
         console.error(error);
-        // can be useful to end loading state and let the user know something went wrong
         this.olympics$.next([]);
         return throwError(() => new Error('Failed to load Olympic data'));
-        // Afficher en alerte l'erreur dans app.component
       })
     );
   }
